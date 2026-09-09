e739545df903899da580b764264a87ac53f0170f
Israel R <he3522@wayne.edu>
2026-09-09 16:49:28 -0400

    Increased to 2500 tokens

--- Veredict_api.py
+++ Veredict_api.py
@@ -167,7 +167,7 @@ def generate_veredicto(user_id: str) -> str:
             {"role": "user", "content": user_prompt},
         ],
         temperature= 0.7,
-        max_tokens= 1200,
+        max_tokens= 2500,
     )
 
     return response.choices[0].message.content
